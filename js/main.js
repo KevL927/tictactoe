643a7fb742fa82956481f9693330ae036c7122bc
KevL927 <leekevin@gmail.com>
2017-04-22 11:15:50 -0400

    Add render function and call function in td click event listen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+function render (tttBoard) {
+    var i = 0,
+        j = 0,
+        k = 0;
+
+    while(i <= 8) {
+      if(i === 3 || i === 6) {
+        k = 0;
+        j++;
+      }
+
+      $('#' + j + '-' + k).html(TttBoard[j][k]);
+      k++;
+      i++;
+    }
+}
+
 $(function() {
   var gameState = TTTGame();
 
@@ -6,5 +23,6 @@ $(function() {
 
     gameState.updateTttBoard(clickedTdId);
     gameState.updateCurrPlayerTurn();
+    render(gameState.tttBoard());
   });
 }
