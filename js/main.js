afa0c9dc1e11d0b87acf3bff0dd3f672d1783fa4
KevL927 <leekevin@gmail.com>
2017-04-23 21:14:54 -0400

    Convert into ES6

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
-$(function() {
-  const gameState = TTTGame(),
-        getNumOfWin = gameState.getNumOfWin();
+$(() => {
+  const gameState = TTTGame();
 
-  function tdClickHandler (e) {
-    var clickedTd = e.target,
-        clickedTdId = clickedTd.id,
-        tdText = $(clickedTd).text(),
-        getTttBoard = gameState.getTttBoard();
+  const tdClickHandler = e => {
+    const clickedTd = e.target,
+          clickedTdId = clickedTd.id,
+          tdText = $(clickedTd).text(),
+          getTttBoard = gameState.getTttBoard();
 
     gameState.updateTttBoard(clickedTdId);
     if (tdText.length === 0) gameState.updateCurrPlayerTurn();
     gameState.updateIsWinner(checkWinner(getTttBoard, checkCombination));
     renderBoardAndPlayerTurn(getTttBoard, gameState.getCurrPlayerTurn());
     if(checkWinner(getTttBoard, checkCombination)) {
-      renderScore('player-one', getNumOfWin.playerOne);
-      renderScore('player-two', getNumOfWin.playerTwo);
+      renderScore('player-one', gameState.getNumOfWin().playerOne);
+      renderScore('player-two', gameState.getNumOfWin().playerTwo);
     };
-  }
+  };
 
-  function setNewAndResetGameSetting (tttBoard, currPlayerTurn) {
+  const setNewAndResetGameSetting = (tttBoard, currPlayerTurn) => {
     $('td').on('click', tdClickHandler);
     $('.buttons').css('visibility', 'hidden');
     $('.player-display').text('Player turn: 1');
     renderBoardAndPlayerTurn(tttBoard, currPlayerTurn);
-  }
+  };
 
-  $('.score').on('DOMSubtreeModified',function(){
+  $('.score').on('DOMSubtreeModified', () => {
     $('td').off();
     $('.buttons').css('visibility', 'visible');
     $('.player-display').text(gameState.getIsWinnerStatus() + ' Won!');
   });
 
-  $('.new-game').on('click', function (e) {
+  $('td').on('click', tdClickHandler);
+
+  $('.new-game').on('click', e => {
     gameState.newGame();
     setNewAndResetGameSetting(gameState.getTttBoard(), gameState.getCurrPlayerTurn());
   });
 
-  $('.reset-game').on('click', function (e) {
+  $('.reset-game').on('click', e => {
     gameState.resetGame();
-    renderScore('player-one', getNumOfWin.playerOne);
-    renderScore('player-two', getNumOfWin.playerTwo);
+    renderScore('player-one', gameState.getNumOfWin().playerOne);
+    renderScore('player-two', gameState.getNumOfWin().playerTwo);
     setNewAndResetGameSetting(gameState.getTttBoard(), gameState.getCurrPlayerTurn());
   });
-
-  $('td').on('click', tdClickHandler);
 });
